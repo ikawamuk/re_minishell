a58a3023352f2b1d6bc6b8046e0c6a85565e6eae
ikawamuk <ikawamuk@student.42.fr>
2025-09-10 21:42:31 +0900

    update: ''

--- expand.c
+++ expand.c
@@ -6,7 +6,7 @@
 /*   By: ikawamuk <ikawamuk@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/06 06:50:55 by ikawamuk          #+#    #+#             */
-/*   Updated: 2025/09/10 19:39:17 by ikawamuk         ###   ########.fr       */
+/*   Updated: 2025/09/10 21:42:23 by ikawamuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,12 @@ int expand(t_element_list *element_list, char **envp)
 	quote(element_list); // クオートマーカーセット
 	// word listの展開
 	// word listの単語分割、空文字列削除
-	// redirect word listの展開
-	// redirect word listの単語分割、空文字削除
+		// リダイレクトの展開は実行部でやろう。
+		// redirect word listの展開
+		// redirect word listで単語分割、空文字列が発生した場合はambiguas redirectでエラー
 	// ワイルドカードの展開（グロブ展開）
-	dequote(element_list); // クオート除去
+	if (dequote(element_list) == ERROR)
+		return (ERROR); // クオート除去
 	return (NO_ERROR);
 }
 
